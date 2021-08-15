f7fb2c32924229bf8b49c30e18e6f9574be9cb7d
anvesh <anvesh.b@globeshout.com>
2021-08-15 21:21:40 +0530

    hash rout add

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Route,Router  } from "react-router-dom";
+import {Route,HashRouter as Router,  } from "react-router-dom";
 import { } from "react-router-dom";
 import routers from "./routes";
 
